63726fe01a1513a56ed94777e9513b47e8daa00f
skyvan2013 <keven_2008@163.com>
2015-11-07 23:50:03 +0800

    2015年11月7日23:50:01

--- ems/src/main/java/com/lazyshan/oa/sms/controllers/ProductTypeController.java
+++ ems/src/main/java/com/lazyshan/oa/sms/controllers/ProductTypeController.java
@@ -1,23 +1,15 @@
 package com.lazyshan.oa.sms.controllers;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.lazyshan.oa.sms.common.GsonUtils;
 import com.lazyshan.oa.sms.models.ProductType;
 import com.lazyshan.oa.sms.service.ProductTypeService;
 
@@ -36,16 +28,7 @@ public class ProductTypeController extends BaseController {
 	@ResponseBody
 	public String ajaxListData() {
 		List<ProductType> pts = productTypeService.nestedListAllProductType();
-		Gson gson =  new GsonBuilder()
-		.registerTypeAdapter(ProductType.class, new JsonSerializer<ProductType>() {
-			@Override
-			public JsonElement serialize(ProductType src, Type typeOfSrc,
-					JsonSerializationContext context) {
-				System.out.println("哈哈哈哈哈哈啊"+src.getTypeId());
-				return new JsonPrimitive(src.getTypeId());
-			}
-		}).create();
-		return gson.toJson(pts);
+		return GsonUtils.toJson(pts, "productType");
 	}
 
 }
--- ems/src/main/java/com/lazyshan/oa/sms/dao/ProductTypeDaoImpl.java
+++ ems/src/main/java/com/lazyshan/oa/sms/dao/ProductTypeDaoImpl.java
@@ -3,19 +3,25 @@ package com.lazyshan.oa.sms.dao;
 import java.util.List;
 
 import org.hibernate.FetchMode;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.orm.hibernate4.HibernateCallback;
 import org.springframework.stereotype.Repository;
 
 import com.lazyshan.oa.sms.models.ProductType;
 
 @Repository
+@SuppressWarnings("unchecked")
 public class ProductTypeDaoImpl extends BaseDao<ProductType> implements ProductTypeDao {
 
 	public List<ProductType> nestedListAllProductType() {
 		DetachedCriteria dc = DetachedCriteria.forClass(entityClass);
 		dc.add(Restrictions.eq("typeLevel", 1));
-		dc.setFetchMode("productTypes", FetchMode.JOIN);
-		return (List<ProductType>) getHibernateTemplate().findByCriteria(dc);
+		dc.setFetchMode("productType", FetchMode.DEFAULT);
+		List<ProductType> productTypes = (List<ProductType>) getHibernateTemplate().findByCriteria(dc);
+		return productTypes;
 	}
 }
